04f684ef65e4cf8d250eb513b7c4841c560e2fdf
William Graham <w.m.graham24@gmail.com>
2018-03-09 12:43:12 +0000

    formatted UserLogin.js

---
+++ src/components/auth/UserLogin.js
@@ -0,0 +1,46 @@
+
+import React from 'react';
+import Axios from 'axios';
+
+import UserLoginForm from './UserLoginForm';
+
+import Auth from '../../lib/Auth';
+
+class UserLogin extends React.Component {
+
+  state = {
+    user: {
+      email: '',
+      password: ''
+    }
+  };
+
+  handleChange = ({ target: { name, value } }) => {
+    const user = Object.assign({}, this.state.user, { [name]: value });
+    this.setState({ user });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    Axios
+      .post('/api/login', this.state.user)
+      .then((res) => {
+        console.log(res);
+        Auth.setToken(res.data.token);
+        this.props.history.push('/');
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <UserLoginForm
+        user={this.state.user}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+  }
+}
+
+export default UserLogin;
