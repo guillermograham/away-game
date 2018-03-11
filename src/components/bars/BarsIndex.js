d1e76588158cc35c9bca83844c2d6b2a0cd96160
William Graham <w.m.graham24@gmail.com>
2018-03-11 11:00:37 +0000

    added page scroller to BarsIndex page

--- src/components/bars/BarsIndex.js
+++ src/components/bars/BarsIndex.js
@@ -18,30 +18,79 @@ class BarsIndex extends React.Component {
         console.log(this.state);
       }))
       .catch(err => console.log(err));
+
+  }
+
+  componentDidMount() {
+
+    var targetOffset, currentPosition,
+      body = document.body,
+      button = document.getElementById('scrollButton'),
+      animateTime = 900;
+
+    function getPageScroll() {
+      var yScroll;
+
+      if (window.pageYOffset) {
+        yScroll = window.pageYOffset;
+      } else if (document.documentElement && document.documentElement.scrollTop) {
+        yScroll = document.documentElement.scrollTop;
+      } else if (document.body) {
+        yScroll = document.body.scrollTop;
+      }
+      return yScroll;
+    }
+
+    button.addEventListener('click', function (event) {
+
+      targetOffset = document.getElementById(event.target.hash.substr(1)).offsetTop;
+      currentPosition = getPageScroll();
+
+      body.classList.add('in-transition');
+      body.style.WebkitTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+      body.style.MozTransform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+      body.style.transform = "translate(0, -" + (targetOffset - currentPosition) + "px)";
+
+      window.setTimeout(function () {
+        body.classList.remove('in-transition');
+        body.style.cssText = "";
+        window.scrollTo(0, targetOffset);
+      }, animateTime);
+
+      event.preventDefault();
+
+    }, false);
   }
 
   render() {
     return (
       <div>
-        { this.state.bars.length > 0 && <GoogleMap bars={this.state.bars} /> }
-        <div className="row">
-          <div className="page-banner col-md-12">
-            { Auth.isAuthenticated() && <Link to="/bars/new" className="main-button">
-              Add Bar
-            </Link> }
+        <div>
+          { this.state.bars.length > 0 && <GoogleMap bars={this.state.bars} /> }
+          <a id="scrollButton" href="#target">Click me!</a>
+        </div>
+        <div id="target">
+          <h2>Bars</h2>
+          <div className="row">
+            <div className="page-banner col-md-12">
+              { Auth.isAuthenticated() && <Link to="/bars/new" className="main-button">
+                Add Bar
+              </Link> }
+            </div>
+            {this.state.bars.map(bar => {
+              return(
+                <div key={bar._id} className="image-tile col-md-3 col-sm-6 col-xs-12">
+                  <Link to={`/bars/${bar._id}`}>
+                    <img src={bar.imageOne} className="img-responsive index" />
+                    <p>{bar.name}</p>
+                  </Link>
+                </div>
+              );
+            })}
           </div>
-          {this.state.bars.map(bar => {
-            return(
-              <div key={bar._id} className="image-tile col-md-3 col-sm-6 col-xs-12">
-                <Link to={`/bars/${bar._id}`}>
-                  <img src={bar.imageOne} className="img-responsive index" />
-                  <p>{bar.name}</p>
-                </Link>
-              </div>
-            );
-          })}
         </div>
       </div>
+
     );
   }
 }
